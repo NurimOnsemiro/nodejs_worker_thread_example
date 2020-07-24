523e8d50422fc995a61ab039188b8132bb93105e
gshong <hgs33hgs@gmail.com>
2020-07-24 21:40:24 +0900

    싱글스레드에서 출력 부분 시간측정 제외

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const envInfo = require('./env_info.json');
-const {Worker} = require('worker_threads');
+const {
+    Worker
+} = require('worker_threads');
 
 let workerPath = path.join(__dirname, './worker.js');
+let resultArr = new Array(envInfo.numThreads + 1);
 
 if (envInfo.useThread) {
     for (let i = 0; i < envInfo.numThreads; i++) {
@@ -16,9 +19,13 @@ if (envInfo.useThread) {
 } else {
     console.time('main');
     for (let i = 0; i < envInfo.numThreads; i++) {
-        doSomething();
+        resultArr[i] = doSomething();
     }
     console.timeEnd('main');
+
+    for (let i = 0; i < envInfo.numThreads; i++) {
+        console.log(`result : ${resultArr[i]}`);
+    }
 }
 
 function doSomething() {
@@ -26,5 +33,5 @@ function doSomething() {
     for (let i = 0; i < envInfo.jobSize; i++) {
         data += i;
     }
-    console.log(`result : ${data}`);
+    return data;
 }
